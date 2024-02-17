0231f56205f9368f1e33d4b7ea7be017a4f20360
Zhengkang Liang <108710292+InfLight9999@users.noreply.github.com>
2024-02-16 23:46:18 -0500

    Add files via upload

---
+++ Numerical Studies/Leave one out prediction/R code and results for other methods for leave one out prediction/indep normal/Boosting p10.R
@@ -0,0 +1,190 @@
+library(readr)
+library(MASS)
+library(randomForest)
+library(gbm)
+library(MAVE)
+library(mda)
+library(readxl)
+library(pracma)
+library(psych)
+
+sample.size<-1000
+dims=10
+result.output<-array(0, dim=c(7,3))
+
+
+design1<-as.matrix(read_csv("data/data_setting1_p10_DesignIndNorm.csv",col_names = FALSE))
+design1B<-read_csv("data/data_setting1_p10_DesignIndNorm.csv",col_names = FALSE)
+a<-matrix(design1, ncol = ncol(design1), dimnames = NULL)
+
+c<-array(0, dim=c(sample.size,(dims+1),100))
+
+for (i in 1:100)
+{
+  c[,,i]<-a[(sample.size*(i-1)+1):(sample.size*i),]
+}
+
+boosts<-1:100
+for (i in 1:100)
+{
+  boosting<-gbm(formula=X11~.,data = design1B[((i-1)*sample.size+1):(sample.size*i-1),], n.trees = 160)
+  yhat.boost<-predict(boosting,newdata = design1B[(sample.size*i),1:dims], n.trees = 160)
+  boosts[i]<-mean((yhat.boost-c[sample.size,(dims+1),i])^2)
+}
+result.output[1,1]<-mean(boosts)
+result.output[1,2]<-std(boosts)
+result.output[1,3]<-median(boosts)
+
+
+
+
+design2<-as.matrix(read_csv("data/data_setting2_p10_DesignIndNorm.csv",col_names = FALSE))
+design2B<-read_csv("data/data_setting2_p10_DesignIndNorm.csv",col_names = FALSE)
+a<-matrix(design2, ncol = ncol(design2), dimnames = NULL)
+
+c<-array(0, dim=c(sample.size,(dims+1),100))
+
+for (i in 1:100)
+{
+  c[,,i]<-a[(sample.size*(i-1)+1):(sample.size*i),]
+}
+
+boosts<-1:100
+for (i in 1:100)
+{
+  boosting<-gbm(formula=X11~.,data = design2B[((i-1)*sample.size+1):(sample.size*i-1),], n.trees = 160)
+  yhat.boost<-predict(boosting,newdata = design2B[(sample.size*i),1:dims], n.trees = 160)
+  boosts[i]<-mean((yhat.boost-c[sample.size,(dims+1),i])^2)
+}
+result.output[2,1]<-mean(boosts)
+result.output[2,2]<-std(boosts)
+result.output[2,3]<-median(boosts)
+
+
+
+
+design3<-as.matrix(read_csv("data/data_setting3_p10_DesignIndNorm.csv",col_names = FALSE))
+design3B<-read_csv("data/data_setting3_p10_DesignIndNorm.csv",col_names = FALSE)
+a<-matrix(design3, ncol = ncol(design3), dimnames = NULL)
+
+c<-array(0, dim=c(sample.size,(dims+1),100))
+
+for (i in 1:100)
+{
+  c[,,i]<-a[(sample.size*(i-1)+1):(sample.size*i),]
+}
+
+boosts<-1:100
+for (i in 1:100)
+{
+  boosting<-gbm(formula=X11~.,data = design3B[((i-1)*sample.size+1):(sample.size*i-1),], n.trees = 160)
+  yhat.boost<-predict(boosting,newdata = design3B[(sample.size*i),1:dims], n.trees = 160)
+  boosts[i]<-mean((yhat.boost-c[sample.size,(dims+1),i])^2)
+}
+result.output[3,1]<-mean(boosts)
+result.output[3,2]<-std(boosts)
+result.output[3,3]<-median(boosts)
+
+
+
+
+design4<-as.matrix(read_csv("data/data_setting4_p10_DesignIndNorm.csv",col_names = FALSE))
+design4B<-read_csv("data/data_setting4_p10_DesignIndNorm.csv",col_names = FALSE)
+a<-matrix(design4, ncol = ncol(design4), dimnames = NULL)
+
+c<-array(0, dim=c(sample.size,(dims+1),100))
+
+for (i in 1:100)
+{
+  c[,,i]<-a[(sample.size*(i-1)+1):(sample.size*i),]
+}
+
+boosts<-1:100
+for (i in 1:100)
+{
+  boosting<-gbm(formula=X11~.,data = design4B[((i-1)*sample.size+1):(sample.size*i-1),], n.trees = 160)
+  yhat.boost<-predict(boosting,newdata = design4B[(sample.size*i),1:dims], n.trees = 160)
+  boosts[i]<-mean((yhat.boost-c[sample.size,(dims+1),i])^2)
+}
+result.output[4,1]<-mean(boosts)
+result.output[4,2]<-std(boosts)
+result.output[4,3]<-median(boosts)
+
+
+
+
+design5<-as.matrix(read_csv("data/data_setting5_p10_DesignIndNorm.csv",col_names = FALSE))
+design5B<-read_csv("data/data_setting5_p10_DesignIndNorm.csv",col_names = FALSE)
+a<-matrix(design5, ncol = ncol(design5), dimnames = NULL)
+
+c<-array(0, dim=c(sample.size,(dims+1),100))
+
+for (i in 1:100)
+{
+  c[,,i]<-a[(sample.size*(i-1)+1):(sample.size*i),]
+}
+
+boosts<-1:100
+for (i in 1:100)
+{
+  boosting<-gbm(formula=X11~.,data = design5B[((i-1)*sample.size+1):(sample.size*i-1),], n.trees = 160)
+  yhat.boost<-predict(boosting,newdata = design5B[(sample.size*i),1:dims], n.trees = 160)
+  boosts[i]<-mean((yhat.boost-c[sample.size,(dims+1),i])^2)
+}
+result.output[5,1]<-mean(boosts)
+result.output[5,2]<-std(boosts)
+result.output[5,3]<-median(boosts)
+
+
+
+
+design6<-as.matrix(read_csv("data/data_setting6_p10_DesignIndNorm.csv",col_names = FALSE))
+design6B<-read_csv("data/data_setting6_p10_DesignIndNorm.csv",col_names = FALSE)
+a<-matrix(design6, ncol = ncol(design6), dimnames = NULL)
+
+c<-array(0, dim=c(sample.size,(dims+1),100))
+
+for (i in 1:100)
+{
+  c[,,i]<-a[(sample.size*(i-1)+1):(sample.size*i),]
+}
+
+boosts<-1:100
+for (i in 1:100)
+{
+  boosting<-gbm(formula=X11~.,data = design6B[((i-1)*sample.size+1):(sample.size*i-1),], n.trees = 160)
+  yhat.boost<-predict(boosting,newdata = design6B[(sample.size*i),1:dims], n.trees = 160)
+  boosts[i]<-mean((yhat.boost-c[sample.size,(dims+1),i])^2)
+}
+result.output[6,1]<-mean(boosts)
+result.output[6,2]<-std(boosts)
+result.output[6,3]<-median(boosts)
+
+
+
+
+design7<-as.matrix(read_csv("data/data_setting7_p10_DesignIndNorm.csv",col_names = FALSE))
+design7B<-read_csv("data/data_setting7_p10_DesignIndNorm.csv",col_names = FALSE)
+a<-matrix(design7, ncol = ncol(design7), dimnames = NULL)
+
+c<-array(0, dim=c(sample.size,(dims+1),100))
+
+for (i in 1:100)
+{
+  c[,,i]<-a[(sample.size*(i-1)+1):(sample.size*i),]
+}
+
+boosts<-1:100
+for (i in 1:100)
+{
+  boosting<-gbm(formula=X11~.,data = design7B[((i-1)*sample.size+1):(sample.size*i-1),], n.trees = 160)
+  yhat.boost<-predict(boosting,newdata = design7B[(sample.size*i),1:dims], n.trees = 160)
+  boosts[i]<-mean((yhat.boost-c[sample.size,(dims+1),i])^2)
+}
+result.output[7,1]<-mean(boosts)
+result.output[7,2]<-std(boosts)
+result.output[7,3]<-median(boosts)
+
+
+
+write.csv(as.data.frame(result.output), "boosting performance p10.csv", row.names=FALSE)
---
+++ Numerical Studies/Leave one out prediction/R code and results for other methods for leave one out prediction/indep normal/correlated ppr p10.R
@@ -0,0 +1,181 @@
+library(readr)
+library(MASS)
+library(randomForest)
+library(gbm)
+library(MAVE)
+library(mda)
+library(readxl)
+library(pracma)
+library(psych)
+
+sample.size<-1000
+result.output<-array(0, dim=c(7,6))
+dims<-10
+
+Design1<-as.matrix(read_csv("data/data_setting1_p10_DesignIndNorm.csv",col_names = FALSE))
+
+c<-matrix(Design1, ncol = ncol(Design1), dimnames = NULL)
+
+mse<-1:100
+mse.gcvspline<-1:100
+
+for (j in 1:100)
+{
+  ppr.fit<-ppr(x=c[(1+sample.size*(j-1)):(sample.size*j-1),1:dims], y=c[(1+sample.size*(j-1)):(sample.size*j-1),(dims+1)],nterms=2)
+  ppr.gcvsplinefit<-ppr(x=c[(1+sample.size*(j-1)):(sample.size*j-1),1:dims], y=c[(1+sample.size*(j-1)):(sample.size*j-1),(dims+1)],nterms=2, sm.method = "gcvspline")
+  mse[j]<-mean((predict(ppr.fit,newdata = t(c[(sample.size*j),1:dims]))-c[(sample.size*j),(dims+1)])^2)
+  mse.gcvspline[j]<-mean((predict(ppr.gcvsplinefit,newdata = t(c[(sample.size*j),1:dims]))-c[(sample.size*j),(dims+1)])^2)
+}
+result.output[1,1]<-mean(mse)
+result.output[1,2]<-std(mse)
+result.output[1,3]<-median(mse)
+result.output[1,4]<-mean(mse.gcvspline)
+result.output[1,5]<-std(mse.gcvspline)
+result.output[1,6]<-median(mse.gcvspline)
+
+
+
+
+
+Design2<-as.matrix(read_csv("data/data_setting2_p10_DesignIndNorm.csv",col_names = FALSE))
+
+c<-matrix(Design2, ncol = ncol(Design2), dimnames = NULL)
+
+mse<-1:100
+mse.gcvspline<-1:100
+
+for (j in 1:100)
+{
+  ppr.fit<-ppr(x=c[(1+sample.size*(j-1)):(sample.size*j-1),1:dims], y=c[(1+sample.size*(j-1)):(sample.size*j-1),(dims+1)],nterms=2)
+  ppr.gcvsplinefit<-ppr(x=c[(1+sample.size*(j-1)):(sample.size*j-1),1:dims], y=c[(1+sample.size*(j-1)):(sample.size*j-1),(dims+1)],nterms=2, sm.method = "gcvspline")
+  mse[j]<-mean((predict(ppr.fit,newdata = t(c[(sample.size*j),1:dims]))-c[(sample.size*j),(dims+1)])^2)
+  mse.gcvspline[j]<-mean((predict(ppr.gcvsplinefit,newdata = t(c[(sample.size*j),1:dims]))-c[(sample.size*j),(dims+1)])^2)
+}
+result.output[2,1]<-mean(mse)
+result.output[2,2]<-std(mse)
+result.output[2,3]<-median(mse)
+result.output[2,4]<-mean(mse.gcvspline)
+result.output[2,5]<-std(mse.gcvspline)
+result.output[2,6]<-median(mse.gcvspline)
+
+
+
+
+Design3<-as.matrix(read_csv("data/data_setting3_p10_DesignIndNorm.csv",col_names = FALSE))
+
+c<-matrix(Design3, ncol = ncol(Design3), dimnames = NULL)
+
+mse<-1:100
+mse.gcvspline<-1:100
+
+for (j in 1:100)
+{
+  ppr.fit<-ppr(x=c[(1+sample.size*(j-1)):(sample.size*j-1),1:dims], y=c[(1+sample.size*(j-1)):(sample.size*j-1),(dims+1)],nterms=2)
+  ppr.gcvsplinefit<-ppr(x=c[(1+sample.size*(j-1)):(sample.size*j-1),1:dims], y=c[(1+sample.size*(j-1)):(sample.size*j-1),(dims+1)],nterms=2, sm.method = "gcvspline")
+  mse[j]<-mean((predict(ppr.fit,newdata = t(c[(sample.size*j),1:dims]))-c[(sample.size*j),(dims+1)])^2)
+  mse.gcvspline[j]<-mean((predict(ppr.gcvsplinefit,newdata = t(c[(sample.size*j),1:dims]))-c[(sample.size*j),(dims+1)])^2)
+}
+result.output[3,1]<-mean(mse)
+result.output[3,2]<-std(mse)
+result.output[3,3]<-median(mse)
+result.output[3,4]<-mean(mse.gcvspline)
+result.output[3,5]<-std(mse.gcvspline)
+result.output[3,6]<-median(mse.gcvspline)
+
+
+
+
+Design4<-as.matrix(read_csv("data/data_setting4_p10_DesignIndNorm.csv",col_names = FALSE))
+
+c<-matrix(Design4, ncol = ncol(Design4), dimnames = NULL)
+
+mse<-1:100
+mse.gcvspline<-1:100
+
+for (j in 1:100)
+{
+  ppr.fit<-ppr(x=c[(1+sample.size*(j-1)):(sample.size*j-1),1:dims], y=c[(1+sample.size*(j-1)):(sample.size*j-1),(dims+1)],nterms=2)
+  ppr.gcvsplinefit<-ppr(x=c[(1+sample.size*(j-1)):(sample.size*j-1),1:dims], y=c[(1+sample.size*(j-1)):(sample.size*j-1),(dims+1)],nterms=2, sm.method = "gcvspline")
+  mse[j]<-mean((predict(ppr.fit,newdata = t(c[(sample.size*j),1:dims]))-c[(sample.size*j),(dims+1)])^2)
+  mse.gcvspline[j]<-mean((predict(ppr.gcvsplinefit,newdata = t(c[(sample.size*j),1:dims]))-c[(sample.size*j),(dims+1)])^2)
+}
+result.output[4,1]<-mean(mse)
+result.output[4,2]<-std(mse)
+result.output[4,3]<-median(mse)
+result.output[4,4]<-mean(mse.gcvspline)
+result.output[4,5]<-std(mse.gcvspline)
+result.output[4,6]<-median(mse.gcvspline)
+
+
+
+
+Design5<-as.matrix(read_csv("data/data_setting5_p10_DesignIndNorm.csv",col_names = FALSE))
+
+c<-matrix(Design5, ncol = ncol(Design5), dimnames = NULL)
+
+mse<-1:100
+mse.gcvspline<-1:100
+
+for (j in 1:100)
+{
+  ppr.fit<-ppr(x=c[(1+sample.size*(j-1)):(sample.size*j-1),1:dims], y=c[(1+sample.size*(j-1)):(sample.size*j-1),(dims+1)],nterms=2)
+  ppr.gcvsplinefit<-ppr(x=c[(1+sample.size*(j-1)):(sample.size*j-1),1:dims], y=c[(1+sample.size*(j-1)):(sample.size*j-1),(dims+1)],nterms=2, sm.method = "gcvspline")
+  mse[j]<-mean((predict(ppr.fit,newdata = t(c[(sample.size*j),1:dims]))-c[(sample.size*j),(dims+1)])^2)
+  mse.gcvspline[j]<-mean((predict(ppr.gcvsplinefit,newdata = t(c[(sample.size*j),1:dims]))-c[(sample.size*j),(dims+1)])^2)
+}
+result.output[5,1]<-mean(mse)
+result.output[5,2]<-std(mse)
+result.output[5,3]<-median(mse)
+result.output[5,4]<-mean(mse.gcvspline)
+result.output[5,5]<-std(mse.gcvspline)
+result.output[5,6]<-median(mse.gcvspline)
+
+
+
+
+Design6<-as.matrix(read_csv("data/data_setting6_p10_DesignIndNorm.csv",col_names = FALSE))
+
+c<-matrix(Design6, ncol = ncol(Design6), dimnames = NULL)
+
+mse<-1:100
+mse.gcvspline<-1:100
+
+for (j in 1:100)
+{
+  ppr.fit<-ppr(x=c[(1+sample.size*(j-1)):(sample.size*j-1),1:dims], y=c[(1+sample.size*(j-1)):(sample.size*j-1),(dims+1)],nterms=2)
+  ppr.gcvsplinefit<-ppr(x=c[(1+sample.size*(j-1)):(sample.size*j-1),1:dims], y=c[(1+sample.size*(j-1)):(sample.size*j-1),(dims+1)],nterms=2, sm.method = "gcvspline")
+  mse[j]<-mean((predict(ppr.fit,newdata = t(c[(sample.size*j),1:dims]))-c[(sample.size*j),(dims+1)])^2)
+  mse.gcvspline[j]<-mean((predict(ppr.gcvsplinefit,newdata = t(c[(sample.size*j),1:dims]))-c[(sample.size*j),(dims+1)])^2)
+}
+result.output[6,1]<-mean(mse)
+result.output[6,2]<-std(mse)
+result.output[6,3]<-median(mse)
+result.output[6,4]<-mean(mse.gcvspline)
+result.output[6,5]<-std(mse.gcvspline)
+result.output[6,6]<-median(mse.gcvspline)
+
+
+
+
+Design7<-as.matrix(read_csv("data/data_setting7_p10_DesignIndNorm.csv",col_names = FALSE))
+
+c<-matrix(Design7, ncol = ncol(Design7), dimnames = NULL)
+
+mse<-1:100
+mse.gcvspline<-1:100
+
+for (j in 1:100)
+{
+  ppr.fit<-ppr(x=c[(1+sample.size*(j-1)):(sample.size*j-1),1:dims], y=c[(1+sample.size*(j-1)):(sample.size*j-1),(dims+1)],nterms=2)
+  ppr.gcvsplinefit<-ppr(x=c[(1+sample.size*(j-1)):(sample.size*j-1),1:dims], y=c[(1+sample.size*(j-1)):(sample.size*j-1),(dims+1)],nterms=2, sm.method = "gcvspline")
+  mse[j]<-mean((predict(ppr.fit,newdata = t(c[(sample.size*j),1:dims]))-c[(sample.size*j),(dims+1)])^2)
+  mse.gcvspline[j]<-mean((predict(ppr.gcvsplinefit,newdata = t(c[(sample.size*j),1:dims]))-c[(sample.size*j),(dims+1)])^2)
+}
+result.output[7,1]<-mean(mse)
+result.output[7,2]<-std(mse)
+result.output[7,3]<-median(mse)
+result.output[7,4]<-mean(mse.gcvspline)
+result.output[7,5]<-std(mse.gcvspline)
+result.output[7,6]<-median(mse.gcvspline)
+
+write.csv(as.data.frame(result.output), "ppr performance p10.csv", row.names=FALSE)
