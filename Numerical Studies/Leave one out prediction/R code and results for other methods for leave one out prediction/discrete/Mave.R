e91fe9e1a77355dc75492da09a1362f35103be3a
Zhengkang Liang <108710292+InfLight9999@users.noreply.github.com>
2024-02-16 23:52:42 -0500

    Add files via upload

---
+++ Numerical Studies/Leave one out prediction/R code and results for other methods for leave one out prediction/discrete/Mave.R
@@ -0,0 +1,304 @@
+library(readr)
+library(MASS)
+library(randomForest)
+library(gbm)
+library(MAVE)
+library(mda)
+library(readxl)
+library(pracma)
+library(psych)
+
+sample.size<-500
+
+result.output<-array(0, dim=c(7,3))
+
+
+design1<-as.matrix(read_csv("data/data_setting1_p6_DesignDiscrete.csv",col_names = FALSE))
+
+a<-matrix(design1, ncol = ncol(design1), dimnames = NULL)
+dims=6
+
+c<-array(0, dim=c(sample.size,7,100))
+
+for (i in 1:100)
+{
+  c[,,i]<-a[(sample.size*(i-1)+1):(sample.size*i),]
+}
+
+
+Y_true<-array(0, dim=c(sample.size,100))
+for (i in 1:100)
+{
+  for (j in 1:sample.size)
+  {
+    Y_true[j,i]<-0.8*(c[j,1,i]+c[j,2,i]+c[j,3,i])^2+2*sqrt(abs(0.25*c[j,1,i]+0.25*c[j,5,i]+0.75*c[j,6,i]))
+  }
+}
+
+
+mse_mave<-1:100
+for (turn in 1:100)
+{
+  dim.mave=2
+  dr.mave <- mave(c[1:(sample.size-1),7,turn]~c[1:(sample.size-1),1:6,turn], method = 'MEANMAVE', max.dim = dim.mave)
+  x.train.mave <- mave.data(dr.mave, x = c[1:(sample.size-1),1:6,turn] , dim = dim.mave)
+  x.test.mave <- mave.data(dr.mave,x = c[sample.size,1:6,turn], dim = dim.mave)
+  model.mars <- mars(x.train.mave, c[1:(sample.size-1),7,turn], degree=dim.mave)
+  y.pred.mars <- predict(model.mars, x.test.mave)
+  mse_mave[turn]<-mean((y.pred.mars - c[sample.size,7,turn])^2)
+}
+
+result.output[1,1]<-mean(mse_mave)
+result.output[1,2]<-std(mse_mave)
+result.output[1,3]<-median(mse_mave)
+
+
+
+design2<-as.matrix(read_csv("data/data_setting2_p6_DesignDiscrete.csv",col_names = FALSE))
+
+a<-matrix(design2, ncol = ncol(design2), dimnames = NULL)
+dims=6
+
+c<-array(0, dim=c(sample.size,7,100))
+
+for (i in 1:100)
+{
+  c[,,i]<-a[(sample.size*(i-1)+1):(sample.size*i),]
+}
+
+
+Y_true<-array(0, dim=c(sample.size,100))
+for (i in 1:100)
+{
+  for (j in 1:sample.size)
+  {
+    Y_true[j,i]<-1.5*exp(0.5*c[j,1,i]+0.5*c[j,4,i]-0.3*c[j,5,i])+8*sin(0.2*c[j,1,i]+0.3*c[j,2,i]+0.5*c[j,3,i]-0.8*c[j,4,i])
+  }
+}
+
+
+mse_mave<-1:100
+for (turn in 1:100)
+{
+  dim.mave=2
+  dr.mave <- mave(c[1:(sample.size-1),7,turn]~c[1:(sample.size-1),1:6,turn], method = 'MEANMAVE', max.dim = dim.mave)
+  x.train.mave <- mave.data(dr.mave, x = c[1:(sample.size-1),1:6,turn] , dim = dim.mave)
+  x.test.mave <- mave.data(dr.mave,x = c[sample.size,1:6,turn], dim = dim.mave)
+  model.mars <- mars(x.train.mave, c[1:(sample.size-1),7,turn], degree=dim.mave)
+  y.pred.mars <- predict(model.mars, x.test.mave)
+  mse_mave[turn]<-mean((y.pred.mars - c[sample.size,7,turn])^2)
+}
+
+result.output[2,1]<-mean(mse_mave)
+result.output[2,2]<-std(mse_mave)
+result.output[2,3]<-median(mse_mave)
+
+
+
+design3<-as.matrix(read_csv("data/data_setting3_p6_DesignDiscrete.csv",col_names = FALSE))
+
+a<-matrix(design3, ncol = ncol(design3), dimnames = NULL)
+dims=6
+
+c<-array(0, dim=c(sample.size,7,100))
+
+for (i in 1:100)
+{
+  c[,,i]<-a[(sample.size*(i-1)+1):(sample.size*i),]
+}
+
+
+Y_true<-array(0, dim=c(sample.size,100))
+for (i in 1:100)
+{
+  for (j in 1:sample.size)
+  {
+    Y_true[j,i]<-1.5*exp(0.5*c[j,1,i]+0.5*c[j,4,i]-0.3*c[j,5,i])+8*log(abs(0.8*c[j,1,i]-0.2*c[j,3,i]+0.5*c[j,6,i])+1)
+  }
+}
+
+
+mse_mave<-1:100
+for (turn in 1:100)
+{
+  dim.mave=2
+  dr.mave <- mave(c[1:(sample.size-1),7,turn]~c[1:(sample.size-1),1:6,turn], method = 'MEANMAVE', max.dim = dim.mave)
+  x.train.mave <- mave.data(dr.mave, x = c[1:(sample.size-1),1:6,turn] , dim = dim.mave)
+  x.test.mave <- mave.data(dr.mave,x = c[sample.size,1:6,turn], dim = dim.mave)
+  model.mars <- mars(x.train.mave, c[1:(sample.size-1),7,turn], degree=dim.mave)
+  y.pred.mars <- predict(model.mars, x.test.mave)
+  mse_mave[turn]<-mean((y.pred.mars - c[sample.size,7,turn])^2)
+}
+
+result.output[3,1]<-mean(mse_mave)
+result.output[3,2]<-std(mse_mave)
+result.output[3,3]<-median(mse_mave)
+
+
+
+design4<-as.matrix(read_csv("data/data_setting4_p6_DesignDiscrete.csv",col_names = FALSE))
+
+a<-matrix(design4, ncol = ncol(design4), dimnames = NULL)
+dims=6
+
+c<-array(0, dim=c(sample.size,7,100))
+
+for (i in 1:100)
+{
+  c[,,i]<-a[(sample.size*(i-1)+1):(sample.size*i),]
+}
+
+
+Y_true<-array(0, dim=c(sample.size,100))
+for (i in 1:100)
+{
+  for (j in 1:sample.size)
+  {
+    Y_true[j,i]<-2*(0.8*c[j,1,i]-0.2*c[j,3,i]+0.5*c[j,6,i])^2+1.5*exp(0.5*c[j,1,i]+0.5*c[j,4,i]-0.3*c[j,5,i])+8*sin(0.2*c[j,1,i]+0.3*c[j,2,i]+0.5*c[j,3,i]-0.8*c[j,4,i])
+  }
+}
+
+
+mse_mave<-1:100
+for (turn in 1:100)
+{
+  dim.mave=3
+  dr.mave <- mave(c[1:(sample.size-1),7,turn]~c[1:(sample.size-1),1:6,turn], method = 'MEANMAVE', max.dim = dim.mave)
+  x.train.mave <- mave.data(dr.mave, x = c[1:(sample.size-1),1:6,turn] , dim = dim.mave)
+  x.test.mave <- mave.data(dr.mave,x = c[sample.size,1:6,turn], dim = dim.mave)
+  model.mars <- mars(x.train.mave, c[1:(sample.size-1),7,turn], degree=dim.mave)
+  y.pred.mars <- predict(model.mars, x.test.mave)
+  mse_mave[turn]<-mean((y.pred.mars - c[sample.size,7,turn])^2)
+}
+
+result.output[4,1]<-mean(mse_mave)
+result.output[4,2]<-std(mse_mave)
+result.output[4,3]<-median(mse_mave)
+
+
+
+
+design5<-as.matrix(read_csv("data/data_setting5_p6_DesignDiscrete.csv",col_names = FALSE))
+
+a<-matrix(design5, ncol = ncol(design5), dimnames = NULL)
+dims=6
+
+c<-array(0, dim=c(sample.size,7,100))
+
+for (i in 1:100)
+{
+  c[,,i]<-a[(sample.size*(i-1)+1):(sample.size*i),]
+}
+
+
+Y_true<-array(0, dim=c(sample.size,100))
+for (i in 1:100)
+{
+  for (j in 1:sample.size)
+  {
+    Y_true[j,i]<-2*(0.8*c[j,1,i]-0.2*c[j,3,i]+0.5*c[j,6,i])^2+1.5*exp(0.5*c[j,1,i]+0.5*c[j,4,i]-0.3*c[j,5,i])+(0.2*c[j,1,i]+0.3*c[j,2,i]+0.5*c[j,3,i]-0.8*c[j,4,i])^3
+  }
+}
+
+
+mse_mave<-1:100
+for (turn in 1:100)
+{
+  dim.mave=3
+  dr.mave <- mave(c[1:(sample.size-1),7,turn]~c[1:(sample.size-1),1:6,turn], method = 'MEANMAVE', max.dim = dim.mave)
+  x.train.mave <- mave.data(dr.mave, x = c[1:(sample.size-1),1:6,turn] , dim = dim.mave)
+  x.test.mave <- mave.data(dr.mave,x = c[sample.size,1:6,turn], dim = dim.mave)
+  model.mars <- mars(x.train.mave, c[1:(sample.size-1),7,turn], degree=dim.mave)
+  y.pred.mars <- predict(model.mars, x.test.mave)
+  mse_mave[turn]<-mean((y.pred.mars - c[sample.size,7,turn])^2)
+}
+
+result.output[5,1]<-mean(mse_mave)
+result.output[5,2]<-std(mse_mave)
+result.output[5,3]<-median(mse_mave)
+
+
+
+design6<-as.matrix(read_csv("data/data_setting6_p6_DesignDiscrete.csv",col_names = FALSE))
+
+a<-matrix(design6, ncol = ncol(design6), dimnames = NULL)
+dims=6
+
+c<-array(0, dim=c(sample.size,7,100))
+
+for (i in 1:100)
+{
+  c[,,i]<-a[(sample.size*(i-1)+1):(sample.size*i),]
+}
+
+
+Y_true<-array(0, dim=c(sample.size,100))
+for (i in 1:100)
+{
+  for (j in 1:sample.size)
+  {
+    Y_true[j,i]<-(0.5*c[j,1,i]+0.5*c[j,4,i]-0.3*c[j,5,i])*(1+0.2*c[j,1,i]+0.3*c[j,2,i]+0.5*c[j,3,i]-0.8*c[j,4,i])
+  }
+}
+
+
+mse_mave<-1:100
+for (turn in 1:100)
+{
+  dim.mave=2
+  dr.mave <- mave(c[1:(sample.size-1),7,turn]~c[1:(sample.size-1),1:6,turn], method = 'MEANMAVE', max.dim = dim.mave)
+  x.train.mave <- mave.data(dr.mave, x = c[1:(sample.size-1),1:6,turn] , dim = dim.mave)
+  x.test.mave <- mave.data(dr.mave,x = c[sample.size,1:6,turn], dim = dim.mave)
+  model.mars <- mars(x.train.mave, c[1:(sample.size-1),7,turn], degree=dim.mave)
+  y.pred.mars <- predict(model.mars, x.test.mave)
+  mse_mave[turn]<-mean((y.pred.mars - c[sample.size,7,turn])^2)
+}
+
+result.output[6,1]<-mean(mse_mave)
+result.output[6,2]<-std(mse_mave)
+result.output[6,3]<-median(mse_mave)
+
+
+
+
+
+design7<-as.matrix(read_csv("data/data_setting7_p6_DesignDiscrete.csv",col_names = FALSE))
+
+a<-matrix(design7, ncol = ncol(design7), dimnames = NULL)
+dims=6
+
+c<-array(0, dim=c(sample.size,7,100))
+
+for (i in 1:100)
+{
+  c[,,i]<-a[(sample.size*(i-1)+1):(sample.size*i),]
+}
+
+
+Y_true<-array(0, dim=c(sample.size,100))
+for (i in 1:100)
+{
+  for (j in 1:sample.size)
+  {
+    Y_true[j,i]<-(0.8*c[j,1,i]-0.2*c[j,3,i]+0.5*c[j,6,i])/(0.5+(1.5+0.5*c[j,1,i]+0.5*c[j,4,i]-0.3*c[j,5,i])^2)
+  }
+}
+
+
+mse_mave<-1:100
+for (turn in 1:100)
+{
+  dim.mave=2
+  dr.mave <- mave(c[1:(sample.size-1),7,turn]~c[1:(sample.size-1),1:6,turn], method = 'MEANMAVE', max.dim = dim.mave)
+  x.train.mave <- mave.data(dr.mave, x = c[1:(sample.size-1),1:6,turn] , dim = dim.mave)
+  x.test.mave <- mave.data(dr.mave,x = c[sample.size,1:6,turn], dim = dim.mave)
+  model.mars <- mars(x.train.mave, c[1:(sample.size-1),7,turn], degree=dim.mave)
+  y.pred.mars <- predict(model.mars, x.test.mave)
+  mse_mave[turn]<-mean((y.pred.mars - c[sample.size,7,turn])^2)
+}
+
+result.output[7,1]<-mean(mse_mave)
+result.output[7,2]<-std(mse_mave)
+result.output[7,3]<-median(mse_mave)
+
+write.csv(as.data.frame(result.output), "mave performance.csv", row.names=FALSE)
---
+++ Numerical Studies/Leave one out prediction/R code and results for other methods for leave one out prediction/discrete/Random Forest p10.R
@@ -0,0 +1,230 @@
+library(readr)
+library(MASS)
+library(randomForest)
+library(gbm)
+library(MAVE)
+library(mda)
+library(readxl)
+library(pracma)
+library(psych)
+
+sample.size<-1000
+dims<-10
+
+result.output<-array(0, dim=c(7,3))
+rfs<-array(0, dim=c(7,dims,100))
+avg_rf<-array(0, dim=c(7,dims))
+std_rf<-array(0, dim=c(7,dims))
+med_rf<-array(0, dim=c(7,dims))
+
+
+design1<-as.matrix(read_csv("data/data_setting1_p10_DesignDiscrete.csv",col_names = FALSE))
+
+a<-matrix(design1, ncol = ncol(design1), dimnames = NULL)
+
+c<-array(0, dim=c(sample.size,(dims+1),100))
+
+for (i in 1:100)
+{
+  c[,,i]<-a[(sample.size*(i-1)+1):(sample.size*i),]
+}
+
+
+for (k in 1:dims) 
+{
+  for (i in 1:100)
+  {
+    forest<-randomForest(formula=X11~.,data = design1[((i-1)*sample.size+1):(sample.size*i-1),],importance=TRUE, mtry=k)
+    yhat.forest<-predict(forest,newdata = t(design1[(sample.size*i),1:dims]))
+    rfs[1,k,i]<-mean((yhat.forest-c[sample.size,(dims+1),i])^2)
+  }
+  avg_rf[1,k]<-mean(rfs[1,k,])
+  std_rf[1,k]<-std(rfs[1,k,])
+  med_rf[1,k]<-median(rfs[1,k,])
+}
+
+
+
+
+design2<-as.matrix(read_csv("data/data_setting2_p10_DesignDiscrete.csv",col_names = FALSE))
+
+a<-matrix(design2, ncol = ncol(design2), dimnames = NULL)
+
+c<-array(0, dim=c(sample.size,(dims+1),100))
+
+for (i in 1:100)
+{
+  c[,,i]<-a[(sample.size*(i-1)+1):(sample.size*i),]
+}
+
+
+for (k in 1:dims) 
+{
+  for (i in 1:100)
+  {
+    forest<-randomForest(formula=X11~.,data = design2[((i-1)*sample.size+1):(sample.size*i-1),],importance=TRUE, mtry=k)
+    yhat.forest<-predict(forest,newdata = t(design2[(sample.size*i),1:dims]))
+    rfs[2,k,i]<-mean((yhat.forest-c[sample.size,(dims+1),i])^2)
+  }
+  avg_rf[2,k]<-mean(rfs[2,k,])
+  std_rf[2,k]<-std(rfs[2,k,])
+  med_rf[2,k]<-median(rfs[2,k,])
+}
+  
+  
+  
+  
+design3<-as.matrix(read_csv("data/data_setting3_p10_DesignDiscrete.csv",col_names = FALSE))
+
+a<-matrix(design3, ncol = ncol(design3), dimnames = NULL)
+
+c<-array(0, dim=c(sample.size,(dims+1),100))
+
+for (i in 1:100)
+{
+  c[,,i]<-a[(sample.size*(i-1)+1):(sample.size*i),]
+}
+
+
+for (k in 1:dims) 
+{
+  for (i in 1:100)
+  {
+    forest<-randomForest(formula=X11~.,data = design3[((i-1)*sample.size+1):(sample.size*i-1),],importance=TRUE, mtry=k)
+    yhat.forest<-predict(forest,newdata = t(design3[(sample.size*i),1:dims]))
+    rfs[3,k,i]<-mean((yhat.forest-c[sample.size,(dims+1),i])^2)
+  }
+  avg_rf[3,k]<-mean(rfs[3,k,])
+  std_rf[3,k]<-std(rfs[3,k,])
+  med_rf[3,k]<-median(rfs[3,k,])
+}
+  
+  
+  
+  
+
+design4<-as.matrix(read_csv("data/data_setting4_p10_DesignDiscrete.csv",col_names = FALSE))
+
+a<-matrix(design4, ncol = ncol(design4), dimnames = NULL)
+
+c<-array(0, dim=c(sample.size,(dims+1),100))
+
+for (i in 1:100)
+{
+  c[,,i]<-a[(sample.size*(i-1)+1):(sample.size*i),]
+}
+
+
+for (k in 1:dims) 
+{
+  for (i in 1:100)
+  {
+    forest<-randomForest(formula=X11~.,data = design4[((i-1)*sample.size+1):(sample.size*i-1),],importance=TRUE, mtry=k)
+    yhat.forest<-predict(forest,newdata = t(design4[(sample.size*i),1:dims]))
+    rfs[4,k,i]<-mean((yhat.forest-c[sample.size,(dims+1),i])^2)
+  }
+  avg_rf[4,k]<-mean(rfs[4,k,])
+  std_rf[4,k]<-std(rfs[4,k,])
+  med_rf[4,k]<-median(rfs[4,k,])
+}
+
+
+
+
+
+design5<-as.matrix(read_csv("data/data_setting5_p10_DesignDiscrete.csv",col_names = FALSE))
+
+a<-matrix(design5, ncol = ncol(design5), dimnames = NULL)
+
+c<-array(0, dim=c(sample.size,(dims+1),100))
+
+for (i in 1:100)
+{
+  c[,,i]<-a[(sample.size*(i-1)+1):(sample.size*i),]
+}
+
+
+for (k in 1:dims) 
+{
+  for (i in 1:100)
+  {
+    forest<-randomForest(formula=X11~.,data = design5[((i-1)*sample.size+1):(sample.size*i-1),],importance=TRUE, mtry=k)
+    yhat.forest<-predict(forest,newdata = t(design5[(sample.size*i),1:dims]))
+    rfs[5,k,i]<-mean((yhat.forest-c[sample.size,(dims+1),i])^2)
+  }
+  avg_rf[5,k]<-mean(rfs[5,k,])
+  std_rf[5,k]<-std(rfs[5,k,])
+  med_rf[5,k]<-median(rfs[5,k,])
+}
+
+
+
+
+
+design6<-as.matrix(read_csv("data/data_setting6_p10_DesignDiscrete.csv",col_names = FALSE))
+
+a<-matrix(design6, ncol = ncol(design6), dimnames = NULL)
+
+c<-array(0, dim=c(sample.size,(dims+1),100))
+
+for (i in 1:100)
+{
+  c[,,i]<-a[(sample.size*(i-1)+1):(sample.size*i),]
+}
+
+
+for (k in 1:dims) 
+{
+  for (i in 1:100)
+  {
+    forest<-randomForest(formula=X11~.,data = design6[((i-1)*sample.size+1):(sample.size*i-1),],importance=TRUE, mtry=k)
+    yhat.forest<-predict(forest,newdata = t(design6[(sample.size*i),1:dims]))
+    rfs[6,k,i]<-mean((yhat.forest-c[sample.size,(dims+1),i])^2)
+  }
+  avg_rf[6,k]<-mean(rfs[6,k,])
+  std_rf[6,k]<-std(rfs[6,k,])
+  med_rf[6,k]<-median(rfs[6,k,])
+}
+
+
+
+
+
+design7<-as.matrix(read_csv("data/data_setting7_p10_DesignDiscrete.csv",col_names = FALSE))
+
+a<-matrix(design7, ncol = ncol(design7), dimnames = NULL)
+
+c<-array(0, dim=c(sample.size,(dims+1),100))
+
+for (i in 1:100)
+{
+  c[,,i]<-a[(sample.size*(i-1)+1):(sample.size*i),]
+}
+
+
+for (k in 1:dims) 
+{
+  for (i in 1:100)
+  {
+    forest<-randomForest(formula=X11~.,data = design7[((i-1)*sample.size+1):(sample.size*i-1),],importance=TRUE, mtry=k)
+    yhat.forest<-predict(forest,newdata = t(design7[(sample.size*i),1:dims]))
+    rfs[7,k,i]<-mean((yhat.forest-c[sample.size,(dims+1),i])^2)
+  }
+  avg_rf[7,k]<-mean(rfs[7,k,])
+  std_rf[7,k]<-std(rfs[7,k,])
+  med_rf[7,k]<-median(rfs[7,k,])
+}
+
+
+
+
+
+
+for (k in 1:7) 
+{
+  result.output[k,1]<-min(avg_rf[k,])
+  result.output[k,2]<-min(std_rf[k,])
+  result.output[k,3]<-min(med_rf[k,])
+}
+
+write.csv(as.data.frame(result.output), "randomforest performance p10.csv", row.names=FALSE)
